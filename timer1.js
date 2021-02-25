93d63fc07f8fb34cd9c8c85683ef64948b818ea0
cadinkor <elliott.thomlison@gmail.com>
2021-02-25 23:30:27 +0000

    Assignment Down

--- timer1.js
+++ timer1.js
@@ -1,15 +1,26 @@
-// process.stdout.write('\x07');
-
-//loop through to produce the beep every x seconds
-//This will make it beep at: 3, 5, 9, 10, 15 seconds (x1000)
-//edge cases to consider: https://web.compass.lighthouselabs.ca/activities/864
-
-const inputArgs = process.argv.slice(2) //process.argv allows to add values within the terminal
+// const inputArgs = process.argv.slice(2) 
 // console.log(inputArgs);
-for (const millis of inputArgs) {
-  setTimeout(() => {
-    console.log(millis)
-    process.stdout.write('\x07'); //sound portion
-  }, millis * 1000); //doing this because the node's entry is single digit and we want it to render into seconds 
-}
+// for (const millis of inputArgs) {
+//   if ( millis < 0){
+//     console.log("Please do not input negative numbers.")
+//     return
+//   } else if (millis === NaN){
+//     console.log("Please input a number.")
+//     return
+//   }
+//    setTimeout(() => {
+//     process.stdout.write('\r' + "*ALARM*")
+//     process.stdout.write('\x07');
+//   }, millis * 1000); 
+// }
 
+const inputArgs = process.argv
+  .slice(2)
+  .filter(time => !isNaN(time))
+  .filter(time => time >= 0);
+
+inputArgs.forEach((time) => {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, time * 1000);
+});
